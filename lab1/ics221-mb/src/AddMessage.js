f3c27a3be914f4fae75264f8cd0471cff81ba5d5
04wakeup <dojun849@gmail.com>
2020-01-22 02:45:37 +0000

    lab2 tsk2 done

--- lab1/ics221-mb/src/AddMessage.js
+++ lab1/ics221-mb/src/AddMessage.js
@@ -4,6 +4,23 @@ import Form from 'react-bootstrap/Form'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Button from 'react-bootstrap/Button'
+import * as yup from 'yup';
+
+const schema = yup.object({
+  name: yup
+  .string()
+  .trim()
+  .min(2, 'Your name must be at least 2 characters!')
+  .max(10, 'Your name cannot be more than 10 characters.')
+  .matches(/[A-za-z]{2,}/, 'Invalid name. Use Upper or Lowercase letters only.')
+  .required('Your name is required.'),
+  msg: yup
+  .string()
+  .trim()
+  .min(3, 'Your message must be at least 3 characters!')
+  .max(20, 'Your message must be no more than 20 characters.')
+  .required('A message is required.')
+  });
 
 class AddMessage extends React.Component {
   
