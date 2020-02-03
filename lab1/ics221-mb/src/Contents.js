df37377d1c52b7c431c4bd158133743c19820080
04wakeup <dojun849@gmail.com>
2020-02-03 17:38:17 +0000

    lab3 done

--- lab1/ics221-mb/src/Contents.js
+++ lab1/ics221-mb/src/Contents.js
@@ -7,20 +7,48 @@ class Contents extends React.Component {
      super(props);
      this.state = {
         MESSAGES:[
-          {order: '1', name: 'Bill', msg: 'Hi All!'}, 
-          {order: '2', name: 'Ann', msg: 'ICS 221 is fun'}
+          // {order: '1', name: 'Bill', msg: 'Hi All!'}, 
+          // {order: '2', name: 'Ann', msg: 'ICS 221 is fun'}
         ]
      };
      this.newMessageCallback = this.newMessageCallback.bind(this);
  }
+ 
+ componentDidMount(){  
+  fetch('http://10.21.75.38:3004/messages') 
+      .then(response => response.json())
+      .then((data) => {
+          this.setState({MESSAGES: data})
+          console.log(this.state.MESSAGES)
+      })
+      .catch(console.log)
+}
 
  newMessageCallback = (values) => { 
         // this.state.MESSAGES.unshift(values); option 1
-        this.setState(
-            {MESSAGES:[values, ...this.state.MESSAGES]}   // option 2
-        );
-        console.log(this.state.MESSAGES); 
-        // console.log(this.state.MESSAGES.length); 
+        // this.setState(  // 이하 4줄 살리고 !!!
+        //     {MESSAGES:[values, ...this.state.MESSAGES]}   // option 2
+        // );  
+        (async () => {
+          class HTTPError extends Error {}
+        
+          const response = await fetch('http://10.21.75.38:3004/messages', {
+            method: 'POST',
+            body: JSON.stringify({name: values.name, msg: values.msg}),
+            headers: {
+              'content-type': 'application/json'
+            }
+          });
+        
+          if (!response.ok) {
+            throw new HTTPError('Fetch error:', response.statusText);
+          }
+        
+          const parsed = await response.json(); 
+          this.setState(   
+                {MESSAGES: parsed}   // assign new data for refresh
+            );
+        })();
   }
    
     render() { 
