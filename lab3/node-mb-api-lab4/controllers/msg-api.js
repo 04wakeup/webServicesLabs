9c1d5e37c94deba7201544ef962c3a3b292008e0
04wakeup <dojun849@gmail.com>
2020-02-05 02:15:17 +0000

    lab4-task5-done

--- lab3/node-mb-api-lab4/controllers/msg-api.js
+++ lab3/node-mb-api-lab4/controllers/msg-api.js
@@ -1,19 +1,35 @@
 const messageSchema = require('../models/message_schema.js');
 
-messages = [
-    {name: 'Bill', msg: 'Hi All!'}, 
-    {name: 'Ann', msg: 'ICS 221 is fun'}
-  ]
+const mongoose = require('mongoose');
+const messageModel = mongoose.model('message');
+
+// messages = [
+//     {name: 'Bill', msg: 'Hi All!'}, 
+//     {name: 'Ann', msg: 'ICS 221 is fun'}
+//   ]
 
 // Get Request  Hanndler
 const getAllMessages = (req, res) => {
     // res.status(200).send('Successful API GET Request');
     // res.status(200).json(messages);
+
+    /* lab3(ok) -> lab4(new method using DB)
     if (typeof messages == 'object' && Array.isArray(messages)){
         res.status(200).json(messages);
     } else {
          res.status(400).send('Bad Request');
     }
+    */ 
+   messageModel
+    .find()
+    .sort( {'_id': -1} )
+    .exec( (error, messages) => {
+        if (error) {
+            res.status(400).send('Bad Request');
+        } else {
+            res.status(200).json(messages);
+        }
+    });
        
 };
 
