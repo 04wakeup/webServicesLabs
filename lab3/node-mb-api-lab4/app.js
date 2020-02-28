81ebd970f76896f7795dcc1f83e231fa942b3c25
04wakeup <dojun849@gmail.com>
2020-02-28 23:35:39 +0000

    lab7 task1 done

--- lab3/node-mb-api-lab4/app.js
+++ lab3/node-mb-api-lab4/app.js
@@ -1,3 +1,4 @@
+const rateLimit = require('express-rate-limit');
 
 // Dotenv is a zero-dependency module that loads environment variables from a .env file into process.env.
 require('dotenv').config();
@@ -20,12 +21,15 @@ var app = express();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
 // app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, 'public')));
 
 // app.use('/', indexRouter);
 // app.use('/users', usersRouter);
 app.use(cors());
+app.use(rateLimit());
+
 app.use('/', apiRouter);
 
 module.exports = app;
