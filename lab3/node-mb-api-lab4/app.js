e0a546771fbe7fabf0b76f0a2c85a4b6cc5127f3
04wakeup <dojun849@gmail.com>
2020-02-29 18:36:11 +0000

    lab6-task9-done

--- lab3/node-mb-api-lab4/app.js
+++ lab3/node-mb-api-lab4/app.js
@@ -15,9 +15,11 @@ var logger = require('morgan');
 
 require('./db');
 const apiRouter = require('./routes/api_router');
+const passport = reuire('passport');
+
+
 
 var app = express();
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -29,7 +31,7 @@ app.use(express.urlencoded({ extended: false }));
 // app.use('/users', usersRouter);
 app.use(cors());
 app.use(rateLimit());
-
 app.use('/', apiRouter);
+app.use(passport.initialize());
 
 module.exports = app;
