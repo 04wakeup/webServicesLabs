6d15592f4fdac81ff0520cd2475c9c03b22a9e41
04wakeup <dojun849@gmail.com>
2020-02-29 01:33:17 +0000

    lab6-task4-done

--- lab3/node-mb-api-lab4/routes/api_router.js
+++ lab3/node-mb-api-lab4/routes/api_router.js
@@ -2,9 +2,13 @@ const express = require('express');
 const router = express.Router(); // object that handles routing.
 
 const msgAPIController = require('../controllers/msg-api');
+const userAPIController = require('../controllers/user-api');
 
 router.route('/messages')
  .get(msgAPIController.getAllMessages)
  .post(msgAPIController.addNewMessage);
+
+ router.route('/users') 
+ .post(userAPIController.registerNewUser);
 
 module.exports = router;
